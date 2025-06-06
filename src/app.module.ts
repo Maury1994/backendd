ba57927bb412d7c531c2ff6e6ab35b9d5678d353
Maury1994 <119636176+Maury1994@users.noreply.github.com>
2025-06-06 17:15:17 -0500

    bascaa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NotificationsModule } from './notifications/notifications.module';
-
+ 
 @Module({
-  imports: [NotificationsModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      port: 5432,
+      username: 'postgres', // tu usuario de postgres
+      password: 'pNRHxLIfNohIvqWOUAlmeRaMVJIwhcRE',    // tu contraseña
+      database: 'railway',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    NotificationsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {} 
